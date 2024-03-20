93b5f9db0eb6be7728eaa3bf869532a57c86c542
zapZarapAnzeige <you@example.com>
2024-03-20 19:46:49 +0100

    added Tables

--- backend/Docker/scripts/createTables.sql
+++ backend/Docker/scripts/createTables.sql
@@ -1,13 +1,13 @@
 USE trainings_DB;
 
-
 CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT,
-    profile_picture BLOB,
-    user_name VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    expired BOOLEAN DEFAULT FALSE,
-    plz VARCHAR(5) DEFAULT NULL,
-    searching_for_partner BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (user_id)
+    user_id INT AUTO_INCREMENT, profile_picture BLOB, user_name VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, expired BOOLEAN DEFAULT FALSE, plz VARCHAR(5) DEFAULT NULL, searching_for_partner BOOLEAN DEFAULT FALSE, PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Excercises (
+    excercise_id INT AUTO_INCREMENT, excercise_name VARCHAR(255) NOT NULL, description TEXT, constant_unit_of_measure ENUM("SxWdh", "Min") NOT NULL, trackable_unit_of_measure VARCHAR(255), PRIMARY KEY (excercise_id)
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    tag_id INT AUTO_INCREMENT, excercise_id INT, tag_name VARCHAR(255) NOT NULL, is_primary_tag BOOLEAN DEFAULT FALSE, PRIMARY KEY (primary_tag_id), FOREIGN KEY (excercise_id) REFERENCES Excercises (excercise_id) ON DELETE CASCADE
 );
