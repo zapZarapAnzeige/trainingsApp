2c3ffce0cf9f3d09225867ea7f9c0cd0dfc20525
mfalk <mfalk@zeb.de>
2024-06-11 11:04:34 +0200

    corrected punctuation

--- frontend/src/pages/About/components/AboutInterface.tsx
+++ frontend/src/pages/About/components/AboutInterface.tsx
@@ -3,8 +3,8 @@ import { Typography } from "@mui/joy";
 export default function AboutInterface() {
   return (
     <Typography level="h2" fontSize="xl" sx={{ mb: 1, mx: "auto" }}>
-      Wir unterstützen dich mit dieser Trainingsapp dabei deine Gesundheitsziele
-      zu erreichen und ein aktives Leben zu führen.
+      Wir unterstützen dich mit dieser Trainingsapp dabei, deine
+      Gesundheitsziele zu erreichen und ein aktives Leben zu führen.
     </Typography>
   );
 }
